d09dad71dff10560a8917e2124a3e79fdb54ca10
Nidhi Chauhan <nidhikchauhan0411@gmail.com>
2018-11-10 13:19:07 +0530

    Cryptocurrency Project

---
+++ coincap_listings.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+listing_url = 'https://api.coinmarketcap.com/v2/listings/'
+
+request = requests.get(listing_url)
+results = request.json()
+
+#print(json.dumps(results, sort_keys=True, indent=4))
+
+data = results['data']
+
+for currency in data:
+    rank = currency['id']
+    name = currency['name']
+    symbol = currency['symbol']
+    print(str(rank) + ': ' + name + ' (' + symbol + ')')
